f336f9ca2304fda24bcfd578eed1b770770ae0b9
cmv.ml02@gmail.com <cmv.ml02@gmail.com>
2025-02-28 02:26:07 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-
+from anvil.js.window import Quill 
+import anvil.js
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -8,3 +9,15 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    editor = anvil.js.get_dom_node(self.column_panel_1)
+    # add a ColumnPanel in the design view
+    # get the dom node for the ColumnPanel
+    self.quill = Quill( editor, {
+        'modules': { 'toolbar': True },
+        'theme': 'snow'
+    })
+    
+    self.quill.on('text-change', self.text_change)
+    
+  def text_change(self, delta, old_delta, source):
+    print(source, self.quill.getText())

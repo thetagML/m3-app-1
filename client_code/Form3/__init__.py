82b1985533ebb0fd5737a52717234c2d81b23903
cmv.ml02@gmail.com <cmv.ml02@gmail.com>
2025-02-28 20:30:25 +0000

    Created Module1, Package1 and Form3; Edited settings

---
+++ client_code/Form3/__init__.py
@@ -0,0 +1,10 @@
+from ._anvil_designer import Form3Template
+from anvil import *
+
+
+class Form3(Form3Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.

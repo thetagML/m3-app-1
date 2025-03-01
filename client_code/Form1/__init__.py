37665c0561e2e8129ba945ad990e31d8b12d0009
cmv.ml02@gmail.com <cmv.ml02@gmail.com>
2025-03-01 01:00:15 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -3,6 +3,9 @@ from anvil import *
 from anvil.js.window import Quill 
 import anvil.js
 
+token = anvil.secrets.get_secret('xxx')
+print( token )
+
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.

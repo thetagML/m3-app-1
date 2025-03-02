059efd8b4d5ba134b918aa58c86a00379fbcc485
cmv.ml02@gmail.com <cmv.ml02@gmail.com>
2025-03-02 12:32:08 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,6 +11,7 @@ class Form1(Form1Template):
     
         # Any code you write here will run before the form opens.
         editor = anvil.js.get_dom_node(self.column_panel_1)
+        editor2 = anvil.js.get_dom_node(self.column_panel_3)
         jQuery( "#anvil-header" ).remove()
         # add a ColumnPanel in the design view
         # get the dom node for the ColumnPanel
@@ -18,11 +19,18 @@ class Form1(Form1Template):
             'modules': { 'toolbar': True },
             'theme': 'snow'
         })
+        self.quill2 = Quill( editor2, {
+            'modules': { 'toolbar': True },
+            'theme': 'snow'
+        })
         
-        self.quill.on('text-change', self.text_change)
+        self.quill2.on('text-change2', self.text_change)
         
     def text_change(self, delta, old_delta, source):
         print(source, self.quill.getText())
+
+    def text_change2(self, delta, old_delta, source):
+        print(source, self.quill2.getText())
 
     def form_show(self, **event_args):
         """This method is called when the HTML panel is shown on the screen"""

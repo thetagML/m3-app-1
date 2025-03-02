fe1d24a95cb46c73e08d95795ff96fe87c0242cb
cmv.ml02@gmail.com <cmv.ml02@gmail.com>
2025-03-02 01:56:19 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -2,25 +2,24 @@ from ._anvil_designer import Form1Template
 from anvil import *
 from anvil.js.window import Quill 
 import anvil.js
-
-token = anvil.secrets.get_secret('xxx')
-print( token )
+from anvil.js.window import jQuery
 
 class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
-    editor = anvil.js.get_dom_node(self.column_panel_1)
-    # add a ColumnPanel in the design view
-    # get the dom node for the ColumnPanel
-    self.quill = Quill( editor, {
-        'modules': { 'toolbar': True },
-        'theme': 'snow'
-    })
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
     
-    self.quill.on('text-change', self.text_change)
-    
-  def text_change(self, delta, old_delta, source):
-    print(source, self.quill.getText())
+        # Any code you write here will run before the form opens.
+        editor = anvil.js.get_dom_node(self.column_panel_1)
+        jQuery( "#anvil-header" ).remove()
+        # add a ColumnPanel in the design view
+        # get the dom node for the ColumnPanel
+        self.quill = Quill( editor, {
+            'modules': { 'toolbar': True },
+            'theme': 'snow'
+        })
+        
+        self.quill.on('text-change', self.text_change)
+        
+    def text_change(self, delta, old_delta, source):
+        print(source, self.quill.getText())
